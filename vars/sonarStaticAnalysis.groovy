ec0d4dd442db131862d0e8ca469933fac8710527
Suyog Shirke <suyogshirke0497@gmail.com>
2024-02-17 23:26:09 +0530

    New ip updated

--- vars/sonarStaticAnalysis.groovy
+++ vars/sonarStaticAnalysis.groovy
@@ -1,6 +1,6 @@
 def call(credentialsId) {
     withSonarQubeEnv(credentialsId: credentialsId){
-        sh 'mvn clean package sonar:sonar -Dsonar.host.url=http://44.201.227.71:9000'
+        sh 'mvn clean package sonar:sonar -Dsonar.host.url=http://3.91.195.245:9000'
     }
     
 }
